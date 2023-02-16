86a3130d478564a2c9fd30b0adbdc90da4ae6ef2
Rachid <chakor.rachid.1@gmail.com>
2023-02-16 15:48:41 -0500

    Created Models

---
+++ AppCRUDoperation/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppCRUDoperation.Models
+{
+    [Table("Departements", Schema ="HR")]
+    public class Department
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int DepartementId { get; set; }
+
+        [Required]
+        [Display(Name = "Departement Name")]
+        [Column(TypeName = "varchar(200)")]
+        public string DepartementName { get; set; } = string.Empty;
+    }
+}
---
+++ AppCRUDoperation/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppCRUDoperation.Models
+{
+    [Table("Employees", Schema = "HR")]
+    public class Employee
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        [Column(TypeName = "varchar(250)")]
+        public string EmployeeName { get; set; } = string.Empty;
+
+        [Display(Name = "Image User")]
+        [Column(TypeName = "varchar(250)")]
+        public string? ImageUser { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Salary")]
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal Salary { get; set; }
+
+        [Display(Name = "Hiring Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd-MMMM-yyyy}")]
+        public DateTime HiringDate { get; set; }
+
+        [Required]
+        [Display(Name = "National ID")]
+        [MaxLength(4)]
+        [MinLength(4)]
+        [Column(TypeName = "varchar(4)")]
+        public string NationalId { get; set; }
+
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public Department? Department { get; set; }
+    }
+}
