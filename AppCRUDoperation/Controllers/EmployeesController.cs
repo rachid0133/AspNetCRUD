c5495ed34c8760e26ced0098250f91c902a2bc7a
Rachid <chakor.rachid.1@gmail.com>
2023-02-17 16:17:47 -0500

    created EmployeeControler and its view

---
+++ AppCRUDoperation/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using AppCRUDoperation.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace AppCRUDoperation.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context= context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Employees.ToList();
+            return View(result);
+        }
+    }
+}
---
+++ AppCRUDoperation/Migrations/20230217205100_InitializeData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppCRUDoperation.Migrations
+{
+    public partial class InitializeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "HR");
+
+            migrationBuilder.CreateTable(
+                name: "Departements",
+                schema: "HR",
+                columns: table => new
+                {
+                    DepartementId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartementName = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departements", x => x.DepartementId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                schema: "HR",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "varchar(250)", nullable: false),
+                    ImageUser = table.Column<string>(type: "varchar(250)", nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(12,2)", nullable: false),
+                    HiringDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NationalId = table.Column<string>(type: "varchar(4)", maxLength: 4, nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departements_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalSchema: "HR",
+                        principalTable: "Departements",
+                        principalColumn: "DepartementId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentId",
+                schema: "HR",
+                table: "Employees",
+                column: "DepartmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees",
+                schema: "HR");
+
+            migrationBuilder.DropTable(
+                name: "Departements",
+                schema: "HR");
+        }
+    }
+}
