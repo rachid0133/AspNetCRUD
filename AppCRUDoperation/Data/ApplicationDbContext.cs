c53a9a203133695d3c67d2b66f2868860a87ee0b
Rachid <chakor.rachid.1@gmail.com>
2023-02-17 15:34:57 -0500

    created DbContext

---
+++ AppCRUDoperation/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AppCRUDoperation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCRUDoperation.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+           // modelBuilder.Entity<Employee>().ToTable("Employee", "HR");
+        }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
