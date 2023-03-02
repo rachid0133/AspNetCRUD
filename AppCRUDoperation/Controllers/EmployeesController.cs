3f0973f3623bd072d6ad32e710121976bf72ee33
Rachid <chakor.rachid.1@gmail.com>
2023-03-01 21:54:10 -0500

    created Edit and Delete actions

--- AppCRUDoperation/Controllers/EmployeesController.cs
+++ AppCRUDoperation/Controllers/EmployeesController.cs
@@ -30,22 +30,7 @@ namespace AppCRUDoperation.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Employee model)
         {
-            var file = HttpContext.Request.Form.Files;
-            if (file.Count() >0) {
-                //upload image
-                string imageName = Guid.NewGuid().ToString() + Path.GetExtension(file[0].FileName);
-                var fileStream = new FileStream(Path.Combine(@"wwwroot/", "images", imageName), FileMode.Create);
-                file[0].CopyTo(fileStream);
-                model.ImageUser = imageName;
-            }
-            else if(model.ImageUser == null && model.EmployeeId==null) {
-                //Image not uploaded
-                model.ImageUser= "DefaultImage.jpg";
-            }
-            else
-            {
-                model.ImageUser = model.ImageUser;
-            }
+            UploadImage(model);
             if (ModelState.IsValid)
             {
                 _context.Employees.Add(model);
@@ -56,5 +41,62 @@ namespace AppCRUDoperation.Controllers
 
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            ViewBag.Departements = _context.Departments.OrderBy(x => x.DepartementName).ToList();
+            var result = _context.Employees.Find(id);
+            return View("Create", result);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Employee model)
+        {
+            UploadImage(model);
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Departements = _context.Departments.OrderBy(x => x.DepartementName).ToList();
+
+            return View(model);
+        }
+
+        public IActionResult Delete(int? id)
+        {          
+            var result = _context.Employees.Find(id);
+            if (result != null)
+            {
+                _context.Employees.Remove(result);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        private void UploadImage(Employee model)
+        {
+            var file = HttpContext.Request.Form.Files;
+            if (file.Count() > 0)
+            {
+                //upload image
+                string imageName = Guid.NewGuid().ToString() + Path.GetExtension(file[0].FileName);
+                var fileStream = new FileStream(Path.Combine(@"wwwroot/", "images", imageName), FileMode.Create);
+                file[0].CopyTo(fileStream);
+                model.ImageUser = imageName;
+            }
+            else if (model.ImageUser == null && model.EmployeeId == null)
+            {
+                //Image not uploaded
+                model.ImageUser = "DefaultImage.jpg";
+            }
+            else
+            {
+                model.ImageUser = model.ImageUser;
+            }
+        }
+
     }
 }
